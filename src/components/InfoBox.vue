c44c539af467d1c180627ee983dad7b912e62fd8
santocaridi99 <santocaridi99@gmail.com>
2022-02-09 13:20:33 +0100

    implementato sezione infobox

--- src/components/InfoBox.vue
+++ src/components/InfoBox.vue
@@ -41,6 +41,65 @@
         </div>
       </div>
     </div>
+    <!-- sezione dei blog più visti -->
+    <div class="bg-beige">
+      <div class="container">
+        <div class="blogcards">
+          <div class="blog-text">
+            <p>ENJOYABLE INSIGHTS</p>
+            <h1>Most Viewed <span>Best Blogs</span></h1>
+            <h5>&rarr; Become a Better Blogger:Content Planning</h5>
+            <h5>&rarr; Promoting Your Online Business on Pinterest</h5>
+            <h5>&rarr; Gamification and Game-Based Learning</h5>
+            <h5>&rarr; Designing on Online Course from Expert's Perspective</h5>
+            <h5>&rarr; Why Online Courses Are the Future of Education</h5>
+          </div>
+          <!-- blog card -->
+          <div class="bcard">
+            <div class="bimage">
+              <img src="../assets/artist-blog-03-480x352.jpeg" alt="artist" />
+            </div>
+            <!-- testo della card degli artisti -->
+            <div class="artistText">
+              <p>ARTIST</p>
+              <h4>Brush Strokes Energize Trees in Paintings</h4>
+              <!-- sezione della card dove ci sono data e visualizzazioni -->
+              <div class="date-views">
+                <div class="date">
+                  <i class="far fa-calendar-alt"></i>
+                  <span>May 15,2020</span>
+                </div>
+                <div class="views">
+                  <i class="far fa-eye"></i>
+                  <span>688 views</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="bcard">
+            <div class="bimage">
+              <img src="../assets/artist-blog-01-480x352.jpg" alt="artist2" />
+            </div>
+            <!-- testo della card degli artisti -->
+            <div class="artistText">
+              <p>ARTIST</p>
+              <h4>Pocket-Sized Notebooks Hold Miniature Pointings</h4>
+              <!-- sezione della card dove ci sono data e visualizzazioni -->
+              <div class="date-views">
+                <div class="date">
+                  <i class="far fa-calendar-alt"></i>
+                  <span>May 15,2020</span>
+                </div>
+                <div class="views">
+                  <i class="far fa-eye"></i>
+                  <span>603 views</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -68,6 +127,54 @@ export default {};
           color: $MountainMeadow;
           font-weight: bold;
           font-size: 50px;
+        }
+      }
+    }
+  }
+  .bg-beige {
+    background-color: $beige;
+    padding: 100px;
+    .container {
+      .blogcards {
+        display: flex;
+        .blog-text {
+            width: 50;
+            align-self: center;
+          h1 {
+            span {
+              color: $MountainMeadow;
+            }
+          }
+        }
+        .bcard {
+            // width come se ci fossero tre card
+          width: calc(100% / 3 - 70px);
+          margin: 40px;
+          .bimage {
+            background-color: $white;
+            img {
+              width: 100%;
+              height: 100%;
+            }
+          }
+          .artistText {
+            background-color: $white;
+            padding: 40px;
+            p {
+              margin-top: 0;
+            }
+          }
+          .date-views {
+            color: $ShadyLady;
+            font-size: 12px;
+            display: flex;
+            i{
+                margin-right: 5px;
+            }
+            .views{
+                margin-left: 20px;
+            }
+          }
         }
       }
     }
